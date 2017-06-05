a4c2a2b234e6ddfa2b68275e83ce228911553e98
Billy Vong <billy@opstarts.com>
2017-06-05 10:18:22 -0700

    Initial commit

---
+++ rplugin/python3/deoplete/sources/importjs.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from .base import Base
+from logging import getLogger
+from subprocess import Popen, PIPE
+import json
+
+logger = getLogger('deoplete')
+
+class Source(Base):
+    def __init__(self, vim):
+        Base.__init__(self, vim)
+        self.name = 'importjs'
+        self.mark = '[importjs]'
+        self.filetypes = ['javascript', 'javascript.jsx']
+        self.min_pattern_length = 2
+        self.rank = 10000
+        self.input_pattern = '((?:\.|(?:,|:|->)\s+)\w*|\()'
+        self.__completer = Completer(vim)
+
+    def get_complete_position(self, context):
+        return self.__completer.determine_completion_position(context)
+
+    def gather_candidates(self, context):
+        return self.__completer.find_candidates(
+            context
+        )
+
+class Completer(object):
+    def __init__(self, vim):
+        import re
+        self.__vim = vim
+        self.__completion_pattern = re.compile('\w*$')
+
+    def get_bin(self):
+        return 'importjs'
+        return self.__vim.vars['deoplete_import_js#bin']
+
+    def determine_completion_position(self, context):
+        result = self.__completion_pattern.search(context['input'])
+
+        if result is None:
+            return self.__vim.current.window.cursor.col
+
+        return result.start()
+
+    def abbreviate_if_needed(self, text):
+        return (text[:47] + '...') if len(text) > 50 else text
+
+    def find_candidates(self, context):
+        line = str(self.__vim.current.window.cursor[0])
+        column = str(self.__vim.current.window.cursor[1] + 1)
+        word = context["input"]
+        command = [self.get_bin(), 'search', word + '*', context["bufpath"]]
+
+        buf = '\n'.join(self.__vim.current.buffer[:])
+
+        try:
+            process = Popen(command, stdout=PIPE, stdin=PIPE)
+            command_results = process.communicate()[0]
+
+            logger.debug(command_results)
+            if process.returncode != 0:
+                return []
+
+            results = json.loads(command_results.decode('utf-8'))
+
+            return [{
+                'word': x["variableName"],
+                'abbr': self.abbreviate_if_needed(x["variableName"]),
+                'kind': '' if x["hasNamedExports"] else 'd',
+                'menu': x["filePath"] if "filePath" in x else x["importPath"] if "importPath" in x else ""
+                } for x in results]
+        except FileNotFoundError:
+            pass
+
---
+++ rplugin/python3/nvim-import-js/__init__.py
@@ -0,0 +1,35 @@
+import sys
+import os
+import neovim
+sys.path.insert(1, os.path.dirname(__file__))
+
+@neovim.plugin
+class ImportJsHost(object):
+
+    def __init__(self, vim):
+        self.vim = vim
+
+    def relative_file(self):
+        """
+            Return the current file
+        """
+        return self.vim.current.buffer.name
+
+    @neovim.autocmd('CompleteDone', pattern='*.js,*.jsx', eval='expand("<amatch>")', sync=True)
+    def importWord(self, afile):
+        completed_item = self.vim.eval('v:completed_item')
+        if completed_item:
+            self.vim.call("importjs#ExecCommand", "word", completed_item["word"])
+
+    def printError(self, message):
+        self.vim.err_write(message + '\n')
+        # self.vim.command(
+        #     'echohl WarningMsg | echo "' + message + '" | echohl None')
+
+    def log(self, message):
+        """
+        Log message to vim echo
+        """
+        val = "{}".format(message)
+        # self.vim.command('redraws!')
+        self.vim.out_write(val + '\n')
